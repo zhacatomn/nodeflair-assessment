f121fddb04103056f15453a9311464b6adb13657
zhacatomn <zhacatomn@gmail.com>
2023-08-11 15:35:18 +0800

    Add styling for items

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ListItem from "./ListItem";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <main className="w-full h-screen bg-stone-100 flex justify-center p-4">
+            <div
+                className="w-full grid gap-3 h-full"
+                style={{ maxWidth: "1110px", gridTemplateColumns: "3fr 5fr" }}
+            >
+                <div className="flex flex-col gap-3 overflow-auto px-2">
+                    <ListItem />
+                    <ListItem isSelected={true} />
+                    <ListItem />
+                    <ListItem />
+                    <ListItem />
+                    <ListItem />
+                </div>
+                <div className="h-full bg-white rounded-lg p-4 shadow-sm">
+                    We're no strangers to love You know the rules and so do I
+                    (do I) A full commitment's what I'm thinking of You wouldn't
+                    get this from any other guy I just wanna tell you how I'm
+                    feeling Gotta make you understand Never gonna give you up
+                    Never gonna let you down Never gonna run around and desert
+                    you Never gonna make you cry Never gonna say goodbye Never
+                    gonna tell a lie and hurt you We've known each other for so
+                    long Your heart's been aching, but you're too shy to say it
+                    (say it) Inside, we both know what's been going on (going
+                    on) We know the game and we're gonna play it And if you ask
+                    me how I'm feeling Don't tell me you're too blind to see
+                    Never gonna give you up Never gonna let you down Never gonna
+                    run around and desert you Never gonna make you cry Never
+                    gonna say goodbye Never gonna tell a lie and hurt you Never
+                    gonna give you up Never gonna let you down Never gonna run
+                    around and desert you Never gonna make you cry Never gonna
+                    say goodbye Never gonna tell a lie and hurt you We've known
+                    each other for so long Your heart's been aching, but you're
+                    too shy to say it (to say it) Inside, we both know what's
+                    been going on (going on) We know the game and we're gonna
+                    play it I just wanna tell you how I'm feeling Gotta make you
+                    understand Never gonna give you up Never gonna let you down
+                    Never gonna run around and desert you Never gonna make you
+                    cry Never gonna say goodbye Never gonna tell a lie and hurt
+                    you Never gonna give you up Never gonna let you down Never
+                    gonna run around and desert you Never gonna make you cry
+                    Never gonna say goodbye Never gonna tell a lie and hurt you
+                    Never gonna give you up Never gonna let you down Never gonna
+                    run around and desert you Never gonna make you cry Never
+                    gonna say goodbye Never gonna tell a lie and hurt you
+                </div>
+            </div>
+        </main>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
